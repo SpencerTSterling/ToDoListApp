e4354564e3eb89e23e74fb2b384afca292ec8ba1
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-05-23 16:14:49 -0700

    Added class + structure for app in TS/JS

---
+++ ts/main.ts
@@ -0,0 +1,39 @@
+// Create a class
+
+class ToDoItem{
+    title: string; 
+    dueDate: Date;
+    isCompleted: boolean;
+}
+
+/*
+let item = new ToDoItem();
+item.title = "Testing";
+item.dueDate = new Date(2020, 1, 1);
+item.isCompleted = false;
+*/
+
+/**
+ * Checks form data is valid
+ */
+function isValid():boolean{
+
+}
+
+/**
+ * Get all input off form and wrap in
+ * ToDoItem object
+ */
+function getToDoItem():ToDoItem{
+    
+}
+
+/**
+ * Display given ToDoItem
+ * @param item new task
+ */
+function displayToDoItem(item:ToDoItem):void {
+
+}
+
+// TASK: allow user to mark ToDoItem as complete
