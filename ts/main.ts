418a018fd091eff245ffaa78bc236c2203f0f661
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-05-23 17:16:45 -0700

    Implemented basic ToDo methods

--- ts/main.ts
+++ ts/main.ts
@@ -13,11 +13,31 @@ item.dueDate = new Date(2020, 1, 1);
 item.isCompleted = false;
 */
 
+window.onload = function(){
+    let addItem = document.getElementById("add");
+    addItem.onclick = main;
+}
+
+/**
+ * If all input is valid, function gets ToDoItem
+ * and stores it into the item variable
+ * then the item is displayed on the webpage
+ */
+function main(){
+    //console.log("button pressed");
+    if ( isValid() ){
+        let item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
+
+
+
 /**
  * Checks form data is valid
  */
 function isValid():boolean{
-
+    return true;
 }
 
 /**
@@ -25,7 +45,20 @@ function isValid():boolean{
  * ToDoItem object
  */
 function getToDoItem():ToDoItem{
-    
+    let myItem = new ToDoItem();
+    //get title
+    let titleInput = getInput("title");
+    myItem.title = titleInput.value;
+
+    //get due date
+    let dueDateInput = getInput("due-date");
+    myItem.dueDate = new Date(dueDateInput.value);
+
+    //get isCompleted
+    let isCompleted = getInput("is-complete");
+    myItem.isCompleted = isCompleted.checked;
+
+    return myItem;
 }
 
 /**
@@ -33,7 +66,53 @@ function getToDoItem():ToDoItem{
  * @param item new task
  */
 function displayToDoItem(item:ToDoItem):void {
+    // displays item's title as <h3>
+    // ex. <h3>Record lecture</h3>
+    let itemText = document.createElement("h3");
+    itemText.innerText = item.title;
+
+    // displays item's due date as a string in <p>
+    // ex. <p>June 1st 2020</p>
+    let itemDate = document.createElement("p");
+    itemDate.innerText = item.dueDate.toString();
+
+    // ex. <div class="completed></div>
+    let itemDiv = document.createElement("div");
+    if(item.isCompleted){
+        itemDiv.classList.add("completed");
+    }
+
+    /*
+        <div class="completed">         <div>
+            <h3> </h3>                        <h3> </h3>
+            <p>   </P>                        <p>   </p>
+        </div>                          <div>
+    */
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemDate);
+
+    if(item.isCompleted){
+        let completedToDos = document.getElementById("complete-items");
+        completedToDos.appendChild(itemDiv);
+    }
+    else{
+        let incompletedToDos = document.getElementById("incomplete-items");
+        incompletedToDos.appendChild(itemDiv);
+    }
+
 
 }
 
 // TASK: allow user to mark ToDoItem as complete
+
+
+
+
+
+/**
+ * Short-cut for <HTMLInputElement>document.getElementById
+ * @param id id of the HTML Input Element
+ */
+function getInput(id):HTMLInputElement{
+    return <HTMLInputElement>document.getElementById(id);
+}
